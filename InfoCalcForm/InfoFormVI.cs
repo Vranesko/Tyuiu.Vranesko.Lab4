8446c42437a6c18339d038327ebe4d9144cf542f
dant3 <dant3@DANT1>
2024-12-18 00:04:02 +0500

    Add project files.

---
+++ InfoCalcForm/InfoFormVI.cs
@@ -0,0 +1,17 @@
+namespace InfoFormV
+{
+    public partial class InfoFormVI : Form
+    {
+        public InfoFormVI()
+        {
+            InitializeComponent();
+        }
+
+        public static object ApplicationConfiguration { get; internal set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
